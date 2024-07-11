e4d4030ee8ae963f324196280f111ef26e1d316b
Karan Ravindra <karan.ravindra1213@gmail.com>
2024-07-10 21:42:53 -0400

    fix imports

--- ml_zoo/models/__init__.py
+++ ml_zoo/models/__init__.py
@@ -1,6 +1,6 @@
 from .gpt import GPT, GPTConfig
 from .unet import UNet, UNetConfig
 from .vqvae import VQVAE
-import components
+from . import components
 
 __all__ = ["GPT", "GPTConfig", "VQVAE", "UNet", "UNetConfig", "components"]
