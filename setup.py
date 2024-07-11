2f93c5faa854886dc38ed9798ae4f00f0dd0b6d2
Karan Ravindra <karan.ravindra1213@gmail.com>
2024-07-10 22:12:26 -0400

    og versioning

--- setup.py
+++ setup.py
@@ -1,15 +1,29 @@
-from setuptools import setup, find_packages
 import codecs
-import os
+import os.path
+from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
 
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\\n" + fh.read()
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="ml_zoo",
-    version=f"{{ VERSION_PLACEHOLDER }}",
+    version=get_version("ml_zoo/__init__.py"),
     author="Karan Ravindra",
     author_email="contact@karanravindra.com",
     description="A collection of machine learning utilities and models.",
