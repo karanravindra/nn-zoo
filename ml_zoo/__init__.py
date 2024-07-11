7cfe4bf8c7cc3b5618314dc178b5063cc4f85bcd
Karan Ravindra <karan.ravindra1213@gmail.com>
2024-07-10 21:39:26 -0400

    make top levels accessable

--- ml_zoo/__init__.py
+++ ml_zoo/__init__.py
@@ -1,2 +1,8 @@
 __name__ = "ml_zoo"
 __version__ = "0.1.1"
+
+import datamodules as datamodules
+import models as models
+import trainers as trainers
+
+__all__ = ["datamodules", "models", "trainers"]
