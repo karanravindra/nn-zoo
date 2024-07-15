150a10ddc3db0fdf935963254f0358853cc384fd
Karan Ravindra <karan.ravindra1213@gmail.com>
2024-07-15 13:56:42 -0400

    remove some depthwise tests

--- tests/models/components/depthwise_test.py
+++ tests/models/components/depthwise_test.py
@@ -3,17 +3,6 @@ from nn_zoo.models.components.depthwise import *
 
 
 def test_depthwise_conv1d():
-    T, C = 32, 64
-    x = torch.randn(1, C, T)
-
-    conv = DepthwiseConv1d(C, 3)
-    output = conv(x)
-    print(output.shape)
-
-    assert output.shape == x.shape
-
-
-def test_depthwise_conv1d_batch():
     B, T, C = 3, 32, 64
     x = torch.randn(B, C, T)
 
@@ -22,15 +11,6 @@ def test_depthwise_conv1d_batch():
 
 
 def test_depthwise_conv2d():
-    H, W, C = 32, 32, 64
-    x = torch.randn(1, C, H, W)
-
-    conv = DepthwiseConv2d(C, 3)
-
-    assert conv(x).shape == x.shape
-
-
-def test_depthwise_conv2d_batch():
     B, H, W, C = 3, 32, 32, 64
     x = torch.randn(B, C, H, W)
 
@@ -40,15 +20,6 @@ def test_depthwise_conv2d_batch():
 
 
 def test_depthwise_conv3d():
-    D, H, W, C = 32, 32, 32, 64
-    x = torch.randn(1, C, D, H, W)
-
-    conv = DepthwiseConv3d(C, 3)
-
-    assert conv(x).shape == x.shape
-
-
-def test_depthwise_conv3d_batch():
     B, D, H, W, C = 3, 32, 32, 32, 64
     x = torch.randn(B, C, D, H, W)
 
@@ -58,15 +29,6 @@ def test_depthwise_conv3d_batch():
 
 
 def test_depthwise_seperable_conv1d():
-    T, C = 32, 64
-    x = torch.randn(1, C, T)
-
-    conv = DepthwiseSeparabl, 3)Conv1d(C, 3, 3)
-
-    assert conv(x).shape == x.shape
-
-
-def test_depthwise_seperable_conv1d_batch():
     B, T, C = 3, 32, 64
     x = torch.randn(B, C, T)
 
@@ -76,15 +38,6 @@ def test_depthwise_seperable_conv1d_batch():
 
 
 def test_depthwise_seperable_conv2d():
-    H, W, C = 32, 32, 64
-    x = torch.randn(1, C, H, W)
-
-    conv = DepthwiseSeparableConv2d(C, 3, 3)
-
-    assert conv(x).shape == x.shape
-
-
-def test_depthwise_seperable_conv2d_batch():
     B, H, W, C = 3, 32, 32, 64
     x = torch.randn(B, C, H, W)
 
@@ -94,15 +47,6 @@ def test_depthwise_seperable_conv2d_batch():
 
 
 def test_depthwise_seperable_conv3d():
-    D, H, W, C = 32, 32, 32, 64
-    x = torch.randn(1, C, D, H, W)
-
-    conv = DepthwiseSeparableConv3d(C, 3, 3)
-
-    assert conv(x).shape == x.shape
-
-
-def test_depthwise_seperable_conv3d_batch():
     B, D, H, W, C = 3, 32, 32, 32, 64
     x = torch.randn(B, C, D, H, W)
 
