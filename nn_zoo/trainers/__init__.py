3597dc050b0bc366981ee229e2310354c2de9543
Karan Ravindra <karan.ravindra1213@gmail.com>
2024-07-15 15:17:55 -0400

    fix pytest tests

--- nn_zoo/trainers/__init__.py
+++ nn_zoo/trainers/__init__.py
@@ -1,4 +1,3 @@
-from .vqvae_trainer import VQVAE_Trainer
 from .classifier import ClassifierTrainer
 
-__all__ = ["VQVAE_Trainer", "ClassifierTrainer"]
+__all__ = ["ClassifierTrainer"]
--- tests/models/components/depthwise_test.py
+++ tests/models/components/depthwise_test.py
@@ -1,5 +1,12 @@
 import torch
-from nn_zoo.models.components.depthwise import *
+from nn_zoo.models.components.depthwise import (
+    DepthwiseConv1d,
+    DepthwiseConv2d,
+    DepthwiseConv3d,
+    DepthwiseSeparableConv1d,
+    DepthwiseSeparableConv2d,
+    DepthwiseSeparableConv3d,
+)
 
 
 def test_depthwise_conv1d():
@@ -32,7 +39,7 @@ def test_depthwise_seperable_conv1d():
     B, T, C = 3, 32, 64
     x = torch.randn(B, C, T)
 
-    conv = DepthwiseSeparableConv1d(C, 3, 3)
+    conv = DepthwiseSeparableConv1d(C, C, 3)
 
     assert conv(x).shape == x.shape
 
@@ -41,7 +48,7 @@ def test_depthwise_seperable_conv2d():
     B, H, W, C = 3, 32, 32, 64
     x = torch.randn(B, C, H, W)
 
-    conv = DepthwiseSeparableConv2d(C, 3, 3)
+    conv = DepthwiseSeparableConv2d(C, C, 3)
 
     assert conv(x).shape == x.shape
 
@@ -50,6 +57,6 @@ def test_depthwise_seperable_conv3d():
     B, D, H, W, C = 3, 32, 32, 32, 64
     x = torch.randn(B, C, D, H, W)
 
-    conv = DepthwiseSeparableConv3d(C, 3, 3)
+    conv = DepthwiseSeparableConv3d(C, C, 3)
 
     assert conv(x).shape == x.shape
