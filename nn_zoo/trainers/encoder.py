b8d8ff066157e87879db25e1f97ded89b6868109
Karan Ravindra <karan.ravindra1213@gmail.com>
2024-07-15 22:10:34 -0400

    encoder trainer

---
+++ nn_zoo/trainers/encoder.py
@@ -0,0 +1,186 @@
+import torch
+import torch.nn as nn
+import torchvision
+from lightning import LightningDataModule
+from lightning.pytorch import LightningModule
+import torch.optim.optimizer
+from torchmetrics.functional.image.psnr import peak_signal_noise_ratio as psnr
+from torchmetrics.functional.image.ssim import (
+    multiscale_structural_similarity_index_measure as ssim,
+)
+import wandb
+import wandb.plot
+
+__all__ = ["AutoEncoderTrainer"]
+
+
+def get_optim(
+    optim: str,
+) -> type[torch.optim.SGD | torch.optim.Adam | torch.optim.AdamW]:
+    match optim.lower():
+        case "sgd":
+            return torch.optim.SGD
+        case "adam":
+            return torch.optim.Adam
+        case "adamw":
+            return torch.optim.AdamW
+        case _:
+            raise NotImplementedError(
+                f"The requested optimizer: {optim} is not availible"
+            )
+
+
+def get_scheduler(
+    scheduler: str,
+) -> type[
+    torch.optim.lr_scheduler.StepLR
+    | torch.optim.lr_scheduler.MultiStepLR
+    | torch.optim.lr_scheduler.ExponentialLR
+    | torch.optim.lr_scheduler.CosineAnnealingLR
+]:
+    match scheduler.lower():
+        case "steplr":
+            return torch.optim.lr_scheduler.StepLR
+        case "multisteplr":
+            return torch.optim.lr_scheduler.MultiStepLR
+        case "exponentiallr":
+            return torch.optim.lr_scheduler.ExponentialLR
+        case "cosinelr":
+            return torch.optim.lr_scheduler.CosineAnnealingLR
+        case None:
+            return None
+        case _:
+            raise NotImplementedError(
+                f"The requested scheduler: {scheduler} is not availible"
+            )
+
+
+class AutoEncoderTrainer(LightningModule):
+    def __init__(
+        self,
+        model: nn.Module,
+        dm: LightningDataModule,
+        optim: str,
+        optim_kwargs: dict,
+        scheduler: str | None = None,
+        scheduler_args: dict | None = None,
+    ):
+        super(AutoEncoderTrainer, self).__init__()
+        self.model = model
+        self.dm = dm
+        self.optim = get_optim(optim)
+        self.optim_kwargs = optim_kwargs
+        self.scheduler = get_scheduler(scheduler) if scheduler else None
+        self.scheduler_kwargs = scheduler_args if scheduler else None
+
+    def forward(self, x):
+        return self.model(x).output
+
+    def training_step(
+        self, batch: tuple[torch.Tensor, torch.Tensor], batch_idx: int
+    ) -> torch.Tensor:
+        x, y = batch
+
+        out = self.model(x, y)
+        preds, loss = out
+
+        self.log("train_loss", loss)
+        self.log("train_psnr", psnr(preds, y))
+        self.log("train_ssim", ssim(preds, y))
+
+        return loss
+
+    def validation_step(
+        self, batch: tuple[torch.Tensor, torch.Tensor], batch_idx: int
+    ) -> torch.Tensor:
+        x, y = batch
+
+        out = self.model(x, y)
+        preds, loss = out
+
+        self.log("val_loss", loss)
+        self.log("val_psnr", psnr(preds, y))
+        self.log("val_ssim", ssim(preds, y))
+
+        if batch_idx % 10 == 0:
+            self.logger.experiment.log(
+                {
+                    "recon_imgs": wandb.Image(
+                        torchvision.utils.make_grid(preds), caption="Predictions"
+                    ),
+                }
+            )
+
+            if self.current_epoch == 0:
+                self.logger.experiment.log(
+                    {
+                        "original_imgs": wandb.Image(
+                            torchvision.utils.make_grid(y), caption="Original Images"
+                        ),
+                    }
+                )
+
+        return loss
+
+    def test_step(
+        self, batch: tuple[torch.Tensor, torch.Tensor], batch_idx: int
+    ) -> torch.Tensor:
+        x, y = batch
+
+        out = self.model(x, y)
+        preds, loss = out
+
+        self.log("test_loss", loss)
+        self.log("test_psnr", psnr(preds, y))
+        self.log("test_ssim", ssim(preds, y))
+
+        if batch_idx % 10 == 0:
+            self.logger.experiment.log(
+                {
+                    "recon_imgs": wandb.Image(
+                        torchvision.utils.make_grid(preds), caption="Predictions"
+                    ),
+                }
+            )
+
+            if self.current_epoch == 0:
+                self.logger.experiment.log(
+                    {
+                        "negative": wandb.Image(
+                            torchvision.utils.make_grid((x - y)), caption="Negative"
+                        ),
+                    }
+                )
+
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = self.optim(self.model.parameters(), **self.optim_kwargs)
+        scheduler = (
+            self.scheduler(optimizer, **self.scheduler_kwargs)  # type: ignore
+            if self.scheduler
+            else None
+        )
+
+        if scheduler:
+            return [optimizer], [scheduler]
+        else:
+            return optimizer
+
+    def prepare_data(self) -> None:
+        self.dm.prepare_data()
+
+    def setup(self, stage):
+        if stage == "fit":
+            self.dm.setup("fit")
+        elif stage == "test":
+            self.dm.setup("test")
+
+    def train_dataloader(self):
+        return self.dm.train_dataloader()
+
+    def val_dataloader(self):
+        return self.dm.val_dataloader()
+
+    def test_dataloader(self):
+        return self.dm.test_dataloader()
