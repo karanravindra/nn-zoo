af5790e36a61bf454545a62d82b487dd6a00b512
Karan Ravindra <karan.ravindra1213@gmail.com>
2024-07-16 20:41:00 -0400

    add shuffle to datamoudles

--- nn_zoo/datamodules/image/mnist.py
+++ nn_zoo/datamodules/image/mnist.py
@@ -54,7 +54,7 @@ class MNISTDataModule(LightningDataModule):
 
     def train_dataloader(self):
         assert self.train_dataset is not None, f"{self.__class__} not setup properly"
-        return DataLoader(self.train_dataset, **self.loader_params)
+        return DataLoader(self.train_dataset, shuffle=True, **self.loader_params)
 
     def val_dataloader(self):
         assert self.val_dataset is not None, f"{self.__class__} not setup properly"
