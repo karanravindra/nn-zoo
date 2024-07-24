8e344e34ca15fc22f03052a265d3195b4dcab63b
Karan Ravindra <karanravindra1213@gmail.com>
2024-07-24 13:58:37 -0400

    remove v1

--- nn_zoo/datamodules/__init__.py
+++
@@ -1,4 +0,0 @@
-from .cifar import CIFARDataModule
-from .mnist import MNISTDataModule
-
-__all__ = ["CIFARDataModule", "MNISTDataModule"]
--- nn_zoo/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .gpt import GPT, GPTConfig
-from .unet import UNet, UNetConfig
-from .vqvae import VQVAE
-from . import components
-
-__all__ = ["GPT", "GPTConfig", "VQVAE", "UNet", "UNetConfig", "components"]
