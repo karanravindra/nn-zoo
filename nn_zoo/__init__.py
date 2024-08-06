8131b799e2c0dfb2ca2754bef35c82fe10eb4798
Karan Ravindra <karanravindra1213@gmail.com>
2024-08-06 12:48:12 -0400

    fix versioning

--- nn_zoo/__init__.py
+++ nn_zoo/__init__.py
@@ -1,6 +1,10 @@
-__name__ = "nn_zoo"
-__version__ = "1.1.0-alpha.2"
+import tomllib
 
 from . import datamodules, models, trainers
 
+with open("pyproject.toml", "rb") as f:
+    pyproject = tomllib.load(f)
+
+__name__ = "nn_zoo"
+__version__ = pyproject["project"]["version"]
 __all__ = ["datamodules", "models", "trainers"]
